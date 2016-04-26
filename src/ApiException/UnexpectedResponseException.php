1caf6fcfc125be03bf17d71fb3729254136d41a5
Martin Brecht-Precht <mb@markenwerk.net>
2016-04-26 10:26:37 +0200

    Added the UnexpectedResponseException

---
+++ src/ApiException/UnexpectedResponseException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CommonException\ApiException;
+
+/**
+ * Class UnexpectedResponseException
+ *
+ * @package CommonException\ApiException
+ */
+class UnexpectedResponseException extends Base\ApiException
+{
+
+}
